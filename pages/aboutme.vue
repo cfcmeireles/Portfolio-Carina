20000ee0e128627afbf3c74030f5959f2c74c10f
Carina Meireles <carinacmeireles@gmail.com>
2023-08-27 21:25:47 +0100

    Updated styles in aboutme page

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -4,7 +4,7 @@
       <h1 class="section-titles">About me</h1>
       <div class="about-me-text">
         <div>
-          <h2>Hi, I'm <strong>Carina Meireles</strong></h2>
+          <h2>Hi, I'm <strong>Carina</strong></h2>
           <p>
             I'm an aspiring Web Developer with 1 year of learning experience.
             I'm from Lisbon and I love technology and gaming.
@@ -12,36 +12,40 @@
           <p>
             My goal is to develop interactive, responsive software that
             seamlessly engages users and adapts to their needs, while providing
-            invaluable functionality.
+            invaluable functionality. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Tempore, nostrum. Repellat, ratione. Rem placeat
+            quis sequi possimus ea magni corporis?
           </p>
         </div>
         <div>
           <h2>What is my skill level?</h2>
           <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est
+            repudiandae voluptas tenetur veritatis officiis reprehenderit
+            architecto iure corrupti error dolore. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Est repudiandae voluptas tenetur
+            veritatis officiis reprehenderit architecto iure corrupti error
+            dolore.
+          </p>
+          <p>
             At the moment, I am still building a solid foundation in Front-end
-            development, my current skills are:
+            development.
           </p>
         </div>
       </div>
-      <img src="../assets/html-5.png" height="50" />
-      <div class="container">
-        <div class="skills html"></div>
-      </div>
-      <img src="../assets/css-3.png" height="50" />
-      <div class="container">
-        <div class="skills css"></div>
-      </div>
-      <img src="../assets/js.png" height="50" />
-      <div class="container">
-        <div class="skills js"></div>
-      </div>
-      <img src="../assets/vue.png" height="50" />
-      <div class="container">
-        <div class="skills vue"></div>
-      </div>
-      <img src="../assets/nuxt.png" height="30" />
-      <div class="container">
-        <div class="skills nuxt"></div>
+      <h3>My current tech stack:</h3>
+      <div class="gallery">
+        <img class="image" src="../assets/html-5.png" height="80" />
+
+        <img src="../assets/css-3.png" height="80" />
+
+        <img src="../assets/tailwind.png" height="80" />
+
+        <img src="../assets/js.png" height="80" />
+
+        <img src="../assets/vue.png" height="80" />
+
+        <img src="../assets/nuxt-3.png" height="80" />
       </div>
     </div>
   </main>
@@ -53,24 +57,106 @@
 <style>
 @import "../app.css";
 
-.about-me-text {
-  display: grid;
-  grid-template-columns: 600px 600px;
+@media only screen and (min-width: 1451px) {
+  .about-me-text {
+    display: grid;
+    grid-template-columns: 600px 600px;
+  }
+
+  .gallery {
+    margin-top: 50px;
+    display: grid;
+    grid-template-columns: repeat(3, 300px);
+    row-gap: 70px;
+    justify-content: center;
+  }
+
+  .gallery img {
+    display: flex;
+    margin: 0 auto;
+  }
+
+  p {
+    line-height: 1.5;
+  }
 }
 
-.container {
+@media only screen and (max-width: 1450px) {
+  .about-me {
+    width: 50%;
+  }
+
+  .about-me-text {
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+  }
+
+  .gallery {
+    margin-top: 50px;
+    display: grid;
+    grid-template-columns: repeat(3, 200px);
+    row-gap: 20px;
+    justify-content: center;
+  }
+
+  .gallery img {
+    display: flex;
+    height: 60px;
+    margin: 0 auto;
+  }
+
+  p {
+    line-height: 1.5;
+  }
+}
+
+@media only screen and (max-width: 950px) {
+  h1 {
+    margin-top: 100px;
+  }
+
+  .about-me {
+    width: 100%;
+    height: 100%;
+  }
+
+  .about-me-text {
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+  }
+
+  .gallery {
+    margin-top: 50px;
+    display: grid;
+    grid-template-columns: repeat(3, 100px);
+    row-gap: 20px;
+    justify-content: center;
+  }
+
+  .gallery img {
+    display: flex;
+    height: 60px;
+    margin: 0 auto;
+  }
+
+  p {
+    line-height: 1.5;
+  }
+}
+
+/* .container {
   width: 80%;
   background-color: #ddd;
   margin-bottom: 10px;
-}
+} */
 
-.skills {
+/* .skills {
   padding-top: 10px;
   padding-bottom: 10px;
   background-color: blue;
-}
+} */
 
-.html {
+/* .html {
   width: 0;
   animation: html-fill 2s forwards;
 }
@@ -123,5 +209,5 @@
   100% {
     width: 30%;
   }
-}
+} */
 </style>
